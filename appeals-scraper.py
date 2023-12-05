b7c673824916ec06db71c49b4c0b964640f37adb
Kesara Rathnayake <kesara@fq.nz>
2023-12-06 11:43:56 +1300

    fix: Remove supportEmptyParas tags

--- appeals-scraper.py
+++ appeals-scraper.py
@@ -13,10 +13,13 @@ URL = f"{BASE_URL}/iesg/appeal/"
 def save_content(url, filename):
     """Save content as markdown. If image is present, save the PDF as well."""
     response = requests.get(url)
+    for replacement in [b"<![if !supportEmptyParas]>", b"<![endif]>"]:
+        response._content = response.content.replace(replacement, b"")
     soup = BeautifulSoup(response.content, "html.parser")
 
     # find the main content of the page
     main_content = soup.find("div", {"id": "content2"})
+    main_content_str = str(main_content)
 
     # remove exesive lines
     main_content_str = "\n".join(
@@ -30,14 +33,6 @@ def save_content(url, filename):
     with open(f"{filename}.md", "w") as file:
         file.write(markdown)
         print(f"saved to {filename}.md")
-
-    # check if images are present
-    if len(main_content.find_all("img")) > 0:
-        # save PDF file
-        style = "<style>img { max-width: 100%; }</style>"
-        html = f"<html><head>{style}</head><body>{main_content}</body></html>"
-        HTML(string=html, base_url=BASE_URL).write_pdf(f"{filename}.pdf")
-        print(f"saved to {filename}.pdf")
 
 
 response = requests.get(URL)
